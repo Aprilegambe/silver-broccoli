4984857db2f1d6bdc0b3ae43acb524aea54cb32c
Aprilegambe <mancusoantonio952@gmail.com>
2025-05-08 19:48:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,19 @@ package com.mycompany.main;
 
 import javax.swing.JFrame;
 
-public class Main {
+public class Principale {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Battaglia Navale");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(900, 700);
-            frame.setLocationRelativeTo(null);
-            frame.setContentPane(new Menu(frame));
-            frame.setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Creazione della finestra principale del gioco
+                JFrame finestra = new JFrame("Battaglia Navale");
+                finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                finestra.setSize(900, 700); // Dimensioni della finestra
+                finestra.setLocationRelativeTo(null); // Posiziona la finestra al centro dello schermo
+                finestra.setContentPane(new Menu(finestra)); // Imposta il menu come contenuto iniziale
+                finestra.setVisible(true); // Rendi visibile la finestra
+            }
         });
     }
 }
